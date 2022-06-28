cc3ebe5e54dc37a0b0a3600e3a9534c85b42ee14
FarzeenKist <farzeenkistnareddy@gmail.com>
2022-06-28 23:34:42 +0400

    Fixed bug on frontend and improved smart contract

--- contract/marketplace.sol
+++ contract/marketplace.sol
@@ -2,96 +2,132 @@
 
 pragma solidity ^0.8.7;
 
-contract celo{
-    // contains list of people that are signing up to be in the waitlist and we are only allowing 50
-    uint256 public beta_tester ;
-
-    //Total number of whitelist space
-    uint256 public whitelistRemaining = 50;
-
-    //The closing time when the contract is no longer availabe for users to perform transactions with it anymore.
-    uint256 public  closingTime =  1659182400;
+contract celo {
+    //counter for number of beta users
+    mapping(uint256 => uint256) beta_testers;
 
     // Define variable for user who joins
-    user[] public users;
+    mapping(address => User) public users;
+    uint256 public usersCount; // counter for total users registered
 
-    struct user {
-        string handle;
+    struct User {
+        string aliasName;
         address walletaddress;
     }
 
-    //Get the balance of an account, This is what we would use to check if an accout has been added to the waitlist already so they wont take another position as it is limited
-    mapping(address=> uint256) public balances;
-
-
-    //Define variable for user wallet address
-    address userAddress;
-
-    //coonstructor that runs at the creation of this contract/when it is being deployed
-
-    constructor(){
-        userAddress = msg.sender;
+    struct Whitelists {
+        string name;
+        address contractAddress;
+        address host;
+        uint256 spots;
+        uint256 openingTime;
+        uint256 closingTime;
     }
 
+    event NewUser(address indexed user, uint256 number);
+    event NewWhitelist(uint256 number, address contractAddress, address host);
 
-    //Set total Number of allowable users to 20
-    modifier allowUser{
-        require(beta_tester != 21, "Maximum amount of people for the waitlist reached");
+    mapping(uint256 => Whitelists) public whitelists; // maps whitelist events
+    uint256 whitelistsCount;
+    mapping(string => bool) public usedAliases; // ensures unique alias
+    mapping(address => bool) public registered; // used to confirm if msg.sender is registered
+
+    //This is what we would use to check if an accout has been added to the waitlist already so that only one spot is allocated for whitelisting
+    mapping(address => mapping(uint256 => bool)) public isWhitelisted;
+
+    //Define variable for owner
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // Prevents unauthorized users from performing certain actions
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner,
+            "Only accessible to the owner of the smart contract"
+        );
         _;
     }
 
-
-    //This Modifier makes sure that the one account does not tke all the waitlist position
-    //Each account is only given one position in the waitlist
-    modifier OnlyOneAccount {
-        require(balances[msg.sender] != 1, "This user has already been added to the waitlist");
+    modifier onlyHost(uint256 whitelist) {
+        require(
+            msg.sender == whitelists[whitelist].host,
+            "Only host is allowed to do that"
+        );
         _;
     }
-    
 
+    function addWhiteListEvent(
+        address _contractAddress,
+        address _host,
+        string memory _name,
+        uint256 _spots
+    ) public onlyOwner {
+        require(
+            _contractAddress != address(0) && _host != address(0),
+            "Only valid addresses are accepted"
+        );
+        require(bytes(_name).length > 0, "Enter a valid name");
+        require(_spots > 0, "Enter a valid number of spots available");
+        whitelists[whitelistsCount] = Whitelists(
+            _name,
+            _contractAddress,
+            _host,
+            _spots,
+            0,
+            0
+        );
+        emit NewWhitelist(whitelistsCount, _contractAddress, _host);
+        whitelistsCount++;
+    }
 
-    //set when this contract can be used and when it is not usable again with time
-    //This is for cases where the whitelist will open at a particular time and end at a particular time
-     modifier openWhen {
-
-    //When the contract will be opened to being used for transactions
-    uint256  openingTime = 1654911117;
-
-    //The closing time when the contract is no longer availabe for users to perform transactions with it anymore.
-    // uint256  closingTime = 1655739492;
-
-
-         require(block.timestamp >= openingTime && block.timestamp <= closingTime, "You are not yet supposed to use this");
-         _;
-     }
-
-
-    // Add People to the waitlist but first we make sure that the users are not morre than 50 niether is the contract called when it is not in the time range scheduled for the contract to be up and running
-    function addToWaitList
-        (
-            string memory _handle
-        ) 
-        public 
-    allowUser // This handles the check for users not to be more than 50
-    openWhen  // This check for the time when this function can be activated
-    OnlyOneAccount
+    function OpenWhitelist(uint256 whitelist, uint256 _closingTime)
+        public
+        onlyHost(whitelist)
     {
-        balances[msg.sender] += 1;
-        increaseBetaTester();
-        decreaseWhitelist();
-        users.push(user(_handle, userAddress));
+        require(
+            whitelists[whitelist].openingTime == 0 &&
+                whitelists[whitelist].closingTime == 0,
+            "Whitelist hasn't yet been started"
+        );
+        whitelists[whitelist].openingTime = block.timestamp;
+        whitelists[whitelist].closingTime = block.timestamp + _closingTime;
     }
 
-    // Make the increase of users internal, it shouldnt be a function that can be called outside the contract for security purpose
-    function increaseBetaTester()internal{
-        beta_tester++;
-    }
-    //Function to Decrease the Number of Whitelist Available and this number will be dhow in the websuute as available whitelist 
-    function decreaseWhitelist()internal {
-        whitelistRemaining--;
+    // Add user to the waitlist but first we make sure that user isn't already whitelisted and the deadline for closingTime hasn't been reached
+    function addToWaitList(uint256 whitelist) public {
+        require(
+            !isWhitelisted[msg.sender][whitelist],
+            "This user has already been added to the waitlist"
+        );
+        require(
+            whitelists[whitelist].openingTime > 0 &&
+                whitelists[whitelist].closingTime > 0,
+            "Whitelist hasn't yet been started"
+        );
+        require(
+            block.timestamp >= whitelists[whitelist].openingTime &&
+                block.timestamp <= whitelists[whitelist].closingTime,
+            "You are not yet supposed to use this"
+        );
+        require(
+            whitelists[whitelist].spots > 0,
+            "Maximum amount of people for the waitlist reached"
+        );
+
+        isWhitelisted[msg.sender][whitelist] = true;
+        whitelists[whitelist].spots--;
+        beta_testers[whitelist]++;
     }
 
-    function getUserLength() public view returns(uint){
-        return (whitelistRemaining);
+    function registUser(string memory _alias) external {
+        require(!registered[msg.sender], "Already registered");
+        require(bytes(_alias).length > 0, "Enter a valid alias");
+        require(!usedAliases[_alias], "Alias is already taken");
+        users[msg.sender] = User(_alias, msg.sender);
+        usersCount++;
+        emit NewUser(msg.sender, usersCount);
     }
 }
